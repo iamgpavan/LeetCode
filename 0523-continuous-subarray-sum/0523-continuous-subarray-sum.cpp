96786f4cc859b947e9960a2b698b50062e5e9d35
Pavan kumar <58939710+g-pavan@users.noreply.github.com>
2022-10-26 23:56:01 +0530

    Time: 563 ms (23.44%), Space: 113.8 MB (46.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -4,11 +4,11 @@ public:
         unordered_map<int, int> hm;
 	    int sum = 0;
 	    hm[0] = -1;
+        
 	    for(int i=0; i<nums.size(); i++){
 	        sum += nums[i];
 	        int rem = sum%k;
 	        
-	        if(rem<0)  rem += k;
 	         
 	        if(hm.find(rem) != hm.end() && i-hm[rem]>1)
 	            return true;
