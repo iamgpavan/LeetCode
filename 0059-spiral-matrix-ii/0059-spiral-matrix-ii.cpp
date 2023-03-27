e29f529e03d357f8530823e9a6bdf093c126534c
Pavan kumar <58939710+g-pavan@users.noreply.github.com>
2023-03-27 22:37:50 +0530

    Time: 4 ms (32.52%), Space: 6.4 MB (90.31%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> mat(n, vector<int> (n,0));
-        
-        int k = 1;
-        int dir = 0;
-        // 0 - R 
-        // 1 - D
-        // 2 - L
-        // 3 - U
-        
+        vector<vector<int>> mat(n, vector<int> (n,0)); // filled with zeros
+        // 1 - right    2 - down    3 - left    4 - up
+        int value = 1;
         int i = 0;
         int j = 0;
+        int dir = 1;
         
-        while(k <= n*n){
-            mat[i][j] = k;
-            k += 1;
+        while(value <= n*n){
+            mat[i][j] = value;
+            value += 1;
             
-            if(dir == 0){
-                j += 1; 
-                if(j == n || mat[i][j] != 0){ 
+            if(dir == 1){
+                j += 1;
+                // check for edge case that is right corner
+                // 1 -> 2
+                if(j==n || mat[i][j] != 0){
                     j -= 1;
-                    dir = 1;
-                    i += 1;                    
-                }
-            }else if(dir == 1){
-                i += 1;
-                if(i == n || mat[i][j] != 0){
-                    i -= 1;
                     dir = 2;
-                    j -= 1;
+                    i += 1;
                 }
             }else if(dir == 2){
-                j -= 1;
-                if(j == -1 || mat[i][j] != 0){
-                    j += 1;
-                    dir = 3;
+                i += 1;
+                // check for edge case that is dowm corner
+                // 2 -> 3
+                if(i == n || mat[i][j] != 0){
                     i -= 1;
+                    dir = 3;
+                    j -= 1;
                 }
             }else if(dir == 3){
+                j -= 1;
+                // check for edge case that is left corner
+                // 3 -> 4
+                if(j < 0 || mat[i][j] != 0){
+                    j += 1;
+                    dir = 4;
+                    i -= 1;
+                }
+            }else if(dir == 4){
                 i -= 1;
-                if(i == -1 || mat[i][j] != 0){
+                // check for edge case that is upper corner
+                // 4 -> 1
+                if(i < 1 || mat[i][j] != 0){
                     i += 1;
-                    dir = 0;
+                    dir = 1;
                     j += 1;
                 }
             }
         }
         
-        return mat;
+        return mat;        
     }
 };
