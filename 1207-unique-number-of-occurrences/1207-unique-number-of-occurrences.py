63311b3df0d154394c41c3fcf077a7e0d7563ff5
Pavan kumar <pavankumarmasters@gmail.com>
2024-01-17 16:40:53 +0530

    Time: 46 ms (44.70%), Space: 17.6 MB (9.52%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        seen = set()
+
+        for freq in collections.Counter(arr).values():
+
+            if freq in seen:
+
+                return False
+
+            seen.add(freq)
+
+        return True
