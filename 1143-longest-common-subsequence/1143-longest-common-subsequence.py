d9444844cc7626535eebc02bf498469207a14f14
Pavan kumar <pavankumarmasters@gmail.com>
2024-04-26 13:12:40 +0530

    Time: 757 ms (24.88%), Space: 25.8 MB (85.32%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,20 +1,18 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[-1 for j in range(len(text2))] for i in range(len(text1))]
-        # print(dp)
-        
-        def fun(s1, s2, n, m):
-            if(n<0 or m<0):
+        memory = [[-1 for j in range(len(text2))] for i in range(len(text1))]
+        def lcs(i,j, s1, s2, memory):
+            if(i >= len(s1) or j >= len(s2)):
                 return 0
-            # print(n,m)
-            if(dp[n][m] != -1):
-                return dp[n][m]
             
-            if(s1[n] == s2[m]):
-                dp[n][m] = 1 + fun(s1, s2, n-1, m-1)
-            else:
-                dp[n][m] =  max(fun(s1, s2, n-1, m), fun(s1, s2, n, m-1))
+            if(memory[i][j] != -1):
+                return memory[i][j]
             
-            return dp[n][m]
+            if(s1[i] == s2[j]):
+                memory[i][j] = 1 + lcs(i+1, j+1, s1, s2, memory)
+                return memory[i][j] 
+            
+            memory[i][j] = max(lcs(i, j+1, s1, s2, memory), lcs(i+1, j, s1, s2, memory))
+            return memory[i][j]
         
-        return fun(text1, text2, len(text1)-1, len(text2)-1)
+        return lcs(0, 0, text1, text2, memory)
