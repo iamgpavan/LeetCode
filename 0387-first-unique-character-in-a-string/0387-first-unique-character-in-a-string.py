25be72292fece0d785c68c941fb51bb270b193a8
Pavan kumar <pavankumarmasters@gmail.com>
2024-08-03 07:29:40 +0530

    Time: 167 ms (11.06%), Space: 17 MB (94.19%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,12 +1,26 @@
+from collections import deque 
+
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        arr = [0]*26
+        hashmap = [0]*26
+        dq = deque([])
+        i = 0
         
-        for i in range(len(s)):
-            arr[ord(s[i])-ord('a')] += 1
+        while(i < len(s)):
+            ind = ord(s[i]) - ord('a') 
+            # O(1) operation
+            if(hashmap[ind] == 0):
+                dq.append(i)
+            hashmap[ind] += 1
+            
+            while((len(dq)) and hashmap[ord(s[dq[0]]) - ord('a')] > 1):
+                # O(1) operation
+                dq.popleft()
+            
+            i += 1
         
-        for i in range(len(s)):
-            if(arr[ord(s[i])-ord('a')] == 1):
-                return i
+        if(not len(dq)):
+            return -1
         
-        return -1
+        return dq[0]
+        
