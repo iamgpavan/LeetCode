c118cbc7922ff810ba1fa2c204d5520c1235b4d6
Pavan kumar <pavankumarmasters@gmail.com>
2024-08-16 21:52:25 +0530

    Time: 1347 ms (57.18%) | Memory: 41.9 MB (70.69%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,43 +1,21 @@
-class Solution:
-    def maxDistance(self, arrays: List[List[int]]) -> int:
-        first_max = arrays[0][-1]
-        first_max_from = 0
-        
-        first_min = arrays[0][0]
-        first_min_from = 0
-        
-        for i in range(len(arrays)):
-            if(arrays[i][0] < first_min):
-                first_min = arrays[i][0]
-                first_min_from = i
-            if(arrays[i][-1] > first_max):
-                first_max = arrays[i][-1]
-                first_max_from = i
-            
-        second_max = -10**5
-        second_max_from = -1
-        
-        second_min = 10**5
-        second_min_from = -1
+class Solution(object):
+    def maxDistance(self, arrays):
+        """
+        :type arrays: List[List[int]]
+        :rtype: int
+        """
+        global_min = arrays[0][0]
+        global_max = arrays[0][-1]
+        max_distance = 0
 
-        for i in range(len(arrays)):
-            if((i != first_min_from or arrays[i][0] > first_min) and arrays[i][0] < second_min):
-                second_min = arrays[i][0]
-                second_min_from = i
-            if((i != first_max_from or arrays[i][-1] < first_max) and arrays[i][-1] > second_max):
-                second_max = arrays[i][-1]
-                second_max_from = i
-                
-        
+        for i in range(1, len(arrays)):
+            curr_min = arrays[i][0]
+            curr_max = arrays[i][-1]
 
-        if(first_min_from != first_max_from):
-            return abs(first_max-first_min)
+            max_distance = max(max_distance, abs(curr_max - global_min))
+            max_distance = max(max_distance, abs(global_max - curr_min))
 
-        
-        
-        # if(first_max_from != second_min_from):
-        #     return abs(first_max-second_min)
-        
-        return max(abs(first_max - second_min), abs(second_max - first_min))
-        
-        
+            global_min = min(global_min, curr_min)
+            global_max = max(global_max, curr_max)
+
+        return max_distance
